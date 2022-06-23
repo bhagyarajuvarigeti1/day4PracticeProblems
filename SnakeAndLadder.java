afdd21450f837001ff0894fd20d1c8e251fe4bad
bhagyarajuvarigeti1 <bhagyarajuvarigeti@gmail.com>
2022-06-23 06:56:18 +0530

    Snake and Ladder problem solution

---
+++ SnakeAndLadder.java
@@ -0,0 +1,7 @@
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int position = 0;
+        int dieRoll=(int)(Math.random()*7);
+        System.out.println(dieRoll);
+    }
+}
